6c3b3c20fd51f96f72a0562661a70b2e3d3aca92
Daniel Voogsgerd <daniel@voogsgerd.nl>
2018-12-17 22:09:40 +0100

    Making the setup.json path absolute in example

--- detect.py
+++ detect.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import sway_monitors
 import json
-
+import os
 
 def main():
     setup = sway_monitors.Setup()
 
-    with open("setup.json") as f:
+    with open(os.path.join(os.path.dirname(__file__), "setup.json")) as f:
         setups = json.loads(f.read())
 
     setup.check_and_enable_setup(setups["home_setup"])
